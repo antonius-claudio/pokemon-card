257a6763dc5b84b4609566abdd992d50a2c794a7
Claudio Antonius <antonius.claudio@gmail.com>
2020-04-27 19:20:33 +0700

    merapikan

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,28 @@
 import React from 'react';
+import './Card.css';
 
 class Card extends React.Component {
     render() {
         return (
             <>
                 {this.props.cards.map((card) => (
-                    <div key={card.id} className="col s3">
-                        <img src={card.imageUrl} style={style.thumbnail}/>
+                    <div key={card.id} className="col s12 m12 l6 thumbnail animated slideInUp">
+                        <div className="contentCard">
+                            <div className="imageCard">
+                                <img src={card.imageUrl} alt={card.imageUrl}/>
+                            </div>
+                            <div className="descCard">
+                                <h5>{card.name}</h5>
+                                <hr></hr>
+                                <p>Pokedex Number: {card.nationalPokedexNumber ? card.nationalPokedexNumber : "none"}</p>
+                                <p>Types: {card.types ? card.types : "none"}</p>
+                                <p className="desc">Desc: {card.text ? card.text : "none"}</p>
+                            </div>
+                        </div>
                     </div>
                 ))}
             </>
         )
-    }
-}
-
-const style = {
-    thumbnail: {
-        width: '200px'
     }
 }
 
