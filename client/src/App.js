19a74726591020c398fc4b8a9f96cbf2b053bcf0
Claudio Antonius <antonius.claudio@gmail.com>
2020-04-28 19:16:50 +0700

    custom hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,80 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Card from './components/Card';
 import Nav from './components/Nav';
 import './App.css';
+import Detail from './pages/Detail';
+import useFetch from './hooks/useFetch';
 
 export default function App(props) {
-  const [cards, setCards] = useState([]);
+  const [cards, setCards, loading, error] = useFetch(`https://api.pokemontcg.io/v1/cards`);
+  const [myCards, setMyCards] = useState([]);
 
-  useEffect(() => {
-    let page = 1;
-    let url = `https://api.pokemontcg.io/v1/cards?page=${page}`;
-    fetch(url, {
-      method: 'GET'
-    })
-      .then(response => response.json())
-      .then((result) => {
-        setCards(result.cards);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+  function addToMyCard(newData) {
+    setMyCards(myCards.concat(newData));
+  }
+
+  // useEffect(() => {
+  //   let page = 1;
+  //   let url = `https://api.pokemontcg.io/v1/cards?page=${page}`;
+  //   fetch(url, {
+  //     method: 'GET'
+  //   })
+  //     .then(response => response.json())
+  //     .then((result) => {
+  //       setCards(result.cards);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
 
   return (
     <>
       <Nav></Nav>
       <div className="container">
+        {JSON.stringify(myCards)}
+        {cards.cards && <Detail card={cards.cards[0]} addToMyCard={addToMyCard}></Detail>}
+        {cards.cards && <Detail card={cards.cards[1]} addToMyCard={addToMyCard}></Detail>}
         <div className="row">
-          <Card cards={cards}/>
+          {loading && <h5>wait a sec...</h5>}
+          {cards.cards && <Card cards={cards.cards}/>}
         </div>
       </div>
     </>
   );
 }
-
-// import React, { Component } from 'react';
-// import Card from './components/Card';
-// import Nav from './components/Nav';
-// import './App.css';
-
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       cards: []
-//     }
-//   }
-
-//   componentDidMount() {
-//     console.log('DidMount ---->');
-//     let page = 1;
-//     let url = `https://api.pokemontcg.io/v1/cards?page=${page}`;
-//     fetch(url, {
-//       method: 'GET'
-//     })
-//       .then(response => response.json())
-//       .then((result) => {
-//         this.setState({
-//           cards: result.cards
-//         })
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   }
-
-//   render() {
-//     return (
-//       <>
-//         <Nav></Nav>
-//         <div className="container">
-//           <div className="row">
-//             <Card cards={this.state.cards}/>
-//           </div>
-//         </div>
-//       </>
-//     )
-//   }
-// }
-// export default App;
