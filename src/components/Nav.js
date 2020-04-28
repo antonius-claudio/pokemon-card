a63a274731171b3eb1c644f322b6264dac492990
Claudio Antonius <antonius.claudio@gmail.com>
2020-04-28 11:52:28 +0700

    change to functional component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import './Nav.css';
 
-class Nav extends React.Component {
-    render() {
-        return (
-            <>
-                <nav>
-                    <div class="nav-wrapper">
-                    <a href="#" class="brand-logo">Pokemon Card</a>
-                    <ul id="nav-mobile" class="right hide-on-med-and-down">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">MyCard</a></li>
-                    </ul>
-                    </div>
-                </nav>
-            </>
-        )
-    }
+export default function Nav(props) {
+    return (
+        <>
+            <nav>
+                <div class="nav-wrapper">
+                <a href="#" class="brand-logo">Pokemon Card</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">MyCard</a></li>
+                </ul>
+                </div>
+            </nav>
+        </>
+    )
 }
 
-export default Nav;
+// class Nav extends React.Component {
+//     render() {
+//         return (
+//             <>
+//                 <nav>
+//                     <div class="nav-wrapper">
+//                     <a href="#" class="brand-logo">Pokemon Card</a>
+//                     <ul id="nav-mobile" class="right hide-on-med-and-down">
+//                         <li><a href="#">Home</a></li>
+//                         <li><a href="#">MyCard</a></li>
+//                     </ul>
+//                     </div>
+//                 </nav>
+//             </>
+//         )
+//     }
+// }
+
+// export default Nav;
