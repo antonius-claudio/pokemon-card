afd1727812a4172eb2edda10f28e988bea3dadad
Claudio Antonius <antonius.claudio@gmail.com>
2020-04-30 12:53:23 +0700

    home tidak perlu fetch data

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,12 +2,13 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import cardsReducer from './reducers/cards';
 import myCardsReducer from './reducers/myCards';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 const reducer = combineReducers({
     cardsReducer,
     myCardsReducer
 });
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 export default store;
